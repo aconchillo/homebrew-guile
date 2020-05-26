2fb5ad92c947959c8068a2457e1fde1045dc67fe
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-05-26 01:13:41 -0700

    initial commit

---
+++ Formula/guile-json.rb
@@ -0,0 +1,41 @@
+class GuileJson < Formula
+  desc "JSON module for Guile"
+  homepage "https://github.com/aconchillo/guile-json"
+  url "http://download.savannah.gnu.org/releases/guile-json/guile-json-4.0.1.tar.gz"
+  sha256 "609c010573c25601da94f9946fd15aa7d01012a599c284411fec9448a6c24538"
+
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "guile"
+
+  def install
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+    EOS
+  end
+
+  test do
+    json = testpath/"json.scm"
+    json.write <<~EOS
+      (use-modules (json))
+      (json-string->scm "1")
+    EOS
+
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    system "guile", json
+  end
+end
---
+++ README.md
@@ -0,0 +1,17 @@
+# Aleix' Guile Homebrew Tap
+
+## How do I install these formulae?
+
+`brew install aconchillo/guile/<formula>`
+
+Or `brew tap aconchillo/guile` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/aconchillo/homebrew-guile/master/Formula/<formula>.rb
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
