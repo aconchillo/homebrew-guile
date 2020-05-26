15e66522cbd6e270c7d3b086665852a8f2eb2cf2
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-05-26 01:25:54 -0700

    add redis Forumla

---
+++ Formula/guile-redis.rb
@@ -0,0 +1,28 @@
+class GuileRedis < Formula
+  desc "Redis module for Guile"
+  homepage "https://github.com/aconchillo/guile-redis"
+  url "http://download.savannah.gnu.org/releases/guile-redis/guile-redis-1.3.0.tar.gz"
+  sha256 "50e2615f05a62ef304e8c34a4ac7392fe76c29b117fb2b232f22192ba0873b6e"
+
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "guile"
+
+  def install
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+    EOS
+  end
+
+end
