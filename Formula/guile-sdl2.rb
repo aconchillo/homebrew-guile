ec09b451979e7ed8393487f374fb7154b5ca0e0e
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-05-26 14:46:04 -0700

    guile-sdl2: initial commit

---
+++ Formula/guile-sdl2.rb
@@ -0,0 +1,46 @@
+class GuileSdl2 < Formula
+  desc "Guile bindings for the SDL2 C shared library"
+  homepage "https://dthompson.us/projects/guile-sdl2.html"
+  url "https://files.dthompson.us/guile-sdl2/guile-sdl2-0.5.0.tar.gz"
+  sha256 "9564cb72d3d40eabcdfd8f79a0e2fb2c5ddcce52c9150f5df67a7d771e031d85"
+
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "guile"
+  depends_on "sdl2"
+  depends_on "sdl2_image"
+  depends_on "sdl2_mixer"
+  depends_on "sdl2_ttf"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+    EOS
+  end
+
+  test do
+    sdl2 = testpath/"sdl2.scm"
+    sdl2.write <<~EOS
+      (use-modules (sdl2))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    system "guile", sdl2
+  end
+end
