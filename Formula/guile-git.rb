9b74946c114cc7fd35ab575fa3355999fc760ae9
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-05-26 12:04:44 -0700

    guile-git: initial commit

---
+++ Formula/guile-git.rb
@@ -0,0 +1,49 @@
+class GuileGit < Formula
+  desc "Guile-Git is a GNU Guile library providing bindings to libgit2"
+  homepage "https://gitlab.com/guile-git/guile-git"
+  url "https://gitlab.com/guile-git/guile-git/uploads/4c563d8e7e1ff84396abe8ca7011bcaf/guile-git-0.3.0.tar.gz"
+  sha256 "75ce437da024fa57d6a2a48ae4e05d48cb42d84f73a3d372baef5c68421bb130"
+
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texinfo" => :build
+  depends_on "guile"
+  depends_on "scheme-bytestructures"
+  depends_on "libgit2"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    # We need this so we can find scheme-bytestructures module.
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+    EOS
+  end
+
+  test do
+    git = testpath/"git.scm"
+    git.write <<~EOS
+      (use-modules (git))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    system "guile", git
+  end
+end
