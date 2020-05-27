9736b88e12dc1da99cc89153af19f78ad2ff7b6c
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-05-26 17:27:20 -0700

    chickadee: initial commit

---
+++ Formula/chickadee.rb
@@ -0,0 +1,52 @@
+class Chickadee < Formula
+  desc "Module for handling application configuration in a declarative way"
+  homepage "https://dthompson.us/projects/chickadee.html"
+  url "https://files.dthompson.us/chickadee/chickadee-0.5.0.tar.gz"
+  sha256 "5d37903d417d2c2a8555a55bae384490badaeb62d079a511d11e3effc9057a78"
+
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texinfo" => :build
+  depends_on "guile"
+  depends_on "openal-soft"
+  depends_on "libvorbis"
+  depends_on "mpg123"
+  depends_on "guile-sdl2"
+  depends_on "guile-opengl"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    # We need this so we can find other modules.
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+    EOS
+  end
+
+  test do
+    chickadee = testpath/"chickadee.scm"
+    chickadee.write <<~EOS
+      (use-modules (chickadee))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    system "guile", chickadee
+  end
+end
