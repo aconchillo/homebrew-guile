172a9b02ea729838b2ee9fc9698a25bc9d314d7d
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-05-27 00:10:54 -0700

    guile-mastodon: initial commit

---
+++ Formula/guile-mastodon.rb
@@ -0,0 +1,51 @@
+class GuileMastodon < Formula
+  desc "GNU Guile module implementing Mastodon REST API protocol"
+  homepage "https://framagit.org/prouby/guile-mastodon"
+  url "https://framagit.org/prouby/guile-mastodon/-/archive/v0.0.2/guile-mastodon-v0.0.2.tar.gz"
+  sha256 "9b65a202ea9e09f1f8c9c0efd3130545c28a533c12ece1f2def68b8c1fbae4c7"
+
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "guile"
+  depends_on "guile-json"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    # We need this so we can find other modules.
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    inreplace buildpath/"configure.ac", "guile-2.2", "guile-3.0"
+    inreplace buildpath/"configure.ac", "2.2", "3.0"
+    inreplace buildpath/"Makefile.am", "include doc/local.mk", ""
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+    EOS
+  end
+
+  test do
+    mastodon = testpath/"mastodon.scm"
+    mastodon.write <<~EOS
+      (use-modules (mastodon))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    system "guile", mastodon
+  end
+end
