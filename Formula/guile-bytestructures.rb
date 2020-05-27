a91ec44cd8be9e684ce8378acf5ccc416bba2426
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-05-26 18:06:47 -0700

    guile-bytestructures: renamed from scheme-bytestructures

--- Formula/guile-bytestructures.rb
+++ Formula/guile-bytestructures.rb
@@ -1,25 +1,18 @@
-class GuileGit < Formula
-  desc "GNU Guile library providing bindings to libgit2"
-  homepage "https://gitlab.com/guile-git/guile-git"
-  url "https://gitlab.com/guile-git/guile-git/uploads/4c563d8e7e1ff84396abe8ca7011bcaf/guile-git-0.3.0.tar.gz"
-  sha256 "75ce437da024fa57d6a2a48ae4e05d48cb42d84f73a3d372baef5c68421bb130"
+class GuileBytestructures < Formula
+  desc "Library for structured access to bytevector contents"
+  homepage "https://github.com/TaylanUB/scheme-bytestructures"
+  url "https://github.com/TaylanUB/scheme-bytestructures/releases/download/v1.0.7/bytestructures-1.0.7.tar.gz"
+  sha256 "5d354b4d041b30f5a768487ccf6e838d39a54db0b8d73499681bebbc3129bac5"
 
   bottle :unneeded
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
-  depends_on "texinfo" => :build
   depends_on "guile"
-  depends_on "guile-bytestructures"
-  depends_on "libgit2"
 
   def install
     ENV["GUILE_AUTO_COMPILE"] = "0"
-
-    # We need this so we can find scheme-bytestructures module.
-    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
-    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
 
     system "autoreconf", "-vif"
     system "./configure", "--prefix=#{prefix}"
@@ -35,15 +28,15 @@ class GuileGit < Formula
   end
 
   test do
-    git = testpath/"git.scm"
-    git.write <<~EOS
-      (use-modules (git))
+    bytestructures = testpath/"bytestructures.scm"
+    bytestructures.write <<~EOS
+      (use-modules (bytestructures guile))
     EOS
 
     ENV["GUILE_AUTO_COMPILE"] = "0"
     ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
     ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
 
-    system "guile", git
+    system "guile", bytestructures
   end
 end
