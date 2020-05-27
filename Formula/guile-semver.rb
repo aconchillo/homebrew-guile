3745631be22ad89cd8cdda404d1b0f4e18a13f26
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-05-27 00:18:19 -0700

    guile-semver: initial commit

---
+++ Formula/guile-semver.rb
@@ -0,0 +1,42 @@
+class GuileSemver < Formula
+  desc "Guile library to handle Semantic Versions and NPM-style ranges"
+  homepage "https://ngyro.com/software/guile-semver.html"
+  url "https://files.ngyro.com/guile-semver/guile-semver-0.1.1.tar.gz"
+  sha256 "4f790919375feb204a8ea2eda92a291d9bb4de4c8eb1c6776784589a86253781"
+
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "guile"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+    EOS
+  end
+
+  test do
+    semver = testpath/"semver.scm"
+    semver.write <<~EOS
+      (use-modules (semver))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    system "guile", semver
+  end
+end
