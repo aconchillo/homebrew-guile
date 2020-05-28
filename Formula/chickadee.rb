bdbddf6da4e79fe2f04666ae361870ce40e61f1b
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-05-28 00:44:39 -0700

    chickadee: opengl now works properly

--- Formula/chickadee.rb
+++ Formula/chickadee.rb
@@ -55,116 +55,125 @@ end
 
 __END__
 diff --git a/chickadee.scm b/chickadee.scm
-index 4edeb03..1284f26 100644
+index e7fb443..6e40c46 100644
 --- a/chickadee.scm
 +++ b/chickadee.scm
-@@ -232,6 +232,9 @@ border is disabled, otherwise it is enabled.")
+@@ -232,6 +232,11 @@ border is disabled, otherwise it is enabled.")
                     (controller-move (const #t))
                     error)
    (sdl-init)
 +  (sdl2:set-gl-attribute! 'context-major-version 3)
 +  (sdl2:set-gl-attribute! 'context-minor-version 2)
++  ;; context-profile-mask should be set to one of these values:
++  ;; https://github.com/emscripten-ports/SDL2/blob/master/include/SDL_video.h#L231
 +  (sdl2:set-gl-attribute! 'context-profile-mask 1)
    (start-text-input)
    (init-audio)
    (let* ((window (sdl2:make-window #:opengl? #t
 diff --git a/chickadee/render/particles.scm b/chickadee/render/particles.scm
-index d8778da..9970a2f 100644
+index d8778da..0d142fa 100644
 --- a/chickadee/render/particles.scm
 +++ b/chickadee/render/particles.scm
-@@ -116,7 +116,7 @@ indefinitely."
+@@ -116,9 +116,9 @@ indefinitely."
  (define (make-particles-shader)
    (strings->shader
     "
 -#version 130
 +#version 330
- 
- in vec2 position;
+
+-in vec2 position;
++layout (location = 0) in vec2 position;
  in vec2 tex;
+ in vec2 offset;
+ in float life;
 @@ -142,16 +142,17 @@ void main(void) {
  }
  "
     "
 -#version 130
 +#version 330
- 
+
  in vec2 frag_tex;
  in float t;
 +out vec4 fragColor;
  uniform sampler2D color_texture;
  uniform vec4 startColor;
  uniform vec4 endColor;
- 
+
  void main (void) {
 -    gl_FragColor = mix(endColor, startColor, t) * texture2D(color_texture, frag_tex);
 +    fragColor = mix(endColor, startColor, t) * texture(color_texture, frag_tex);
  }
  "))
- 
+
 diff --git a/chickadee/render/pbr.scm b/chickadee/render/pbr.scm
-index 2738b77..650b3cf 100644
+index 2738b77..f1049bc 100644
 --- a/chickadee/render/pbr.scm
 +++ b/chickadee/render/pbr.scm
-@@ -91,7 +91,7 @@
+@@ -91,9 +91,9 @@
    (delay
      (strings->shader
       "
 -#version 130
 +#version 330
- 
- in vec3 position;
+
+-in vec3 position;
++layout (location = 0) in vec3 position;
  in vec2 texcoord0;
-@@ -106,15 +106,16 @@ void main(void) {
+ out vec2 fragTex;
+ uniform mat4 model;
+@@ -106,14 +106,15 @@ void main(void) {
  }
  "
       "
 -#version 130
 +#version 330
- 
+
  in vec2 fragTex;
 +out vec4 fragColor;
  uniform vec3 baseColorFactor;
  uniform sampler2D baseColorTexture;
- 
+
  void main (void) {
 -  gl_FragColor = texture2D(baseColorTexture, fragTex) *
--    vec4(baseColorFactor, 1.0);
 +  fragColor = texture(baseColorTexture, fragTex) *
-+  vec4(baseColorFactor, 1.0);
+     vec4(baseColorFactor, 1.0);
  }
  ")))
- 
 diff --git a/chickadee/render/phong.scm b/chickadee/render/phong.scm
-index 0f2a938..f14041f 100644
+index 0f2a938..f25bf9e 100644
 --- a/chickadee/render/phong.scm
 +++ b/chickadee/render/phong.scm
-@@ -114,7 +114,7 @@
+@@ -114,9 +114,9 @@
    (delay
      (strings->shader
       "
 -#version 130
 +#version 330
- 
- in vec3 position;
+
+-in vec3 position;
++layout (location = 0) in vec3 position;
  in vec2 texcoord;
+ in vec3 normal;
+
 @@ -135,7 +135,7 @@ void main() {
  }
  "
       "
 -#version 130
 +#version 330
- 
+
  struct Material {
    vec3 ambient;
 @@ -162,6 +162,8 @@ struct DirectionalLight {
  in vec3 fragNorm;
  in vec2 fragTex;
- 
+
 +out vec4 fragColor;
 +
  uniform Material material;
  uniform DirectionalLight directionalLight;
- 
+
 @@ -170,20 +172,20 @@ void main() {
      vec3 baseDiffuseColor;
      vec3 baseSpecularColor;
@@ -197,53 +206,59 @@ index 0f2a938..f14041f 100644
 +    fragColor = vec4(ambientColor + diffuseColor + specularColor, 1.0);
  }
  ")))
- 
+
 diff --git a/chickadee/render/shapes.scm b/chickadee/render/shapes.scm
-index f7412e8..e593127 100644
+index f7412e8..b867096 100644
 --- a/chickadee/render/shapes.scm
 +++ b/chickadee/render/shapes.scm
-@@ -58,7 +58,7 @@
+@@ -58,9 +58,9 @@
             (delay
               (strings->shader
                "
 -#version 130
 +#version 330
- 
- in vec2 position;
+
+-in vec2 position;
++layout (location = 0) in vec2 position;
  uniform mat4 mvp;
+
+ void main(void) {
 @@ -68,13 +68,14 @@ void main(void) {
  }
  "
                "
 -#version 130
 +#version 330
- 
+
  in vec2 frag_tex;
 +out vec4 fragColor;
  uniform vec4 color;
- 
+
  void main (void) {
 -    gl_FragColor = color;
 +    fragColor = color;
  }
  ")))
           (mvp (make-null-matrix4)))
-@@ -133,7 +134,7 @@ void main (void) {
+@@ -133,9 +134,9 @@ void main (void) {
             (delay
               (strings->shader
                "
 -#version 130
 +#version 330
- 
- in vec2 position;
+
+-in vec2 position;
++layout (location = 0) in vec2 position;
  in vec2 tex;
+ out vec2 frag_tex;
+ uniform mat4 mvp;
 @@ -146,9 +147,10 @@ void main(void) {
  }
  "
                "
 -#version 130
 +#version 330
- 
+
  in vec2 frag_tex;
 +out vec4 fragColor;
  uniform vec4 color;
@@ -251,7 +266,7 @@ index f7412e8..e593127 100644
  uniform float w;
 @@ -204,9 +206,9 @@ void main (void) {
    }
- 
+
    if (d <= hw) {
 -    gl_FragColor = color;
 +    fragColor = color;
@@ -262,87 +277,93 @@ index f7412e8..e593127 100644
  }
  "))))
 diff --git a/chickadee/render/sprite.scm b/chickadee/render/sprite.scm
-index b6a8232..c2eab96 100644
+index b6a8232..08e026d 100644
 --- a/chickadee/render/sprite.scm
 +++ b/chickadee/render/sprite.scm
-@@ -49,7 +49,7 @@
+@@ -49,9 +49,9 @@
    (delay
      (strings->shader
       "
 -#version 130
 +#version 330
- 
- in vec2 position;
+
+-in vec2 position;
++layout (location = 0) in vec2 position;
  in vec2 tex;
+ out vec2 fragTex;
+ uniform mat4 mvp;
 @@ -62,14 +62,15 @@ void main(void) {
  }
  "
       "
 -#version 130
 +#version 330
- 
+
  in vec2 fragTex;
 +out vec4 fragColor;
  uniform sampler2D colorTexture;
  uniform vec4 tint;
- 
+
  void main (void) {
 -    gl_FragColor = texture2D(colorTexture, fragTex) * tint;
 +    fragColor = texture(colorTexture, fragTex) * tint;
  }
  ")))
- 
-@@ -380,7 +381,7 @@ may be specified via the TEXTURE-REGION argument."
+
+@@ -380,9 +381,9 @@ may be specified via the TEXTURE-REGION argument."
    (delay
      (strings->shader
       "
 -#version 130
 +#version 330
- 
- in vec2 position;
+
+-in vec2 position;
++layout (location = 0) in vec2 position;
  in vec2 tex;
+ in vec4 tint;
+ out vec2 fragTex;
 @@ -396,14 +397,15 @@ void main(void) {
  }
  "
       "
 -#version 130
 +#version 330
- 
+
  in vec2 fragTex;
  in vec4 fragTint;
 +out vec4 fragColor;
  uniform sampler2D colorTexture;
- 
+
  void main (void) {
 -    gl_FragColor = texture2D(colorTexture, fragTex) * fragTint;
 +    fragColor = texture(colorTexture, fragTex) * fragTint;
  }
  ")))
- 
+
 diff --git a/doc/api.texi b/doc/api.texi
 index 9cfa26a..9123916 100644
 --- a/doc/api.texi
 +++ b/doc/api.texi
 @@ -3147,7 +3147,7 @@ Sample vertex shader:
- 
+
  @example
  @verbatim
 -#version 130
 +#version 330
- 
+
  in vec2 position;
  in vec2 tex;
 @@ -3165,13 +3165,14 @@ Sample fragment shader:
- 
+
  @example
  @verbatim
 -#version 130
 +#version 330
- 
+
  in vec2 fragTex;
 +out vec4 fragColor;
  uniform sampler2D colorTexture;
- 
+
  void main (void) {
 -    gl_FragColor = texture2D(colorTexture, fragTex);
 +    fragColor = texture(colorTexture, fragTex);
