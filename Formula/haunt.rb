a56dded2d9257201f25d628bec812711fa0b4292
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-06-23 22:03:37 -0700

    haunt: initial version 0.2.4
    Fixes #1

---
+++ Formula/haunt.rb
@@ -0,0 +1,50 @@
+class Haunt < Formula
+  desc "Simple, functional, hackable static site generator"
+  homepage "https://dthompson.us/projects/haunt.html"
+  url "https://files.dthompson.us/haunt/haunt-0.2.4.tar.gz"
+  sha256 "cce9080a0eca9892613d63ed2514f70bdb180753a2835c4bb603cd19ed27df14"
+
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texinfo" => :build
+  depends_on "guile"
+  depends_on "guile-commonmark"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    # We need this so we can find other modules.
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    inreplace buildpath/"configure.ac", "2.2 2.0", "3.0"
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+    EOS
+  end
+
+  test do
+    haunt = testpath/"haunt.scm"
+    haunt.write <<~EOS
+      (use-modules (haunt site))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    system "guile", haunt
+  end
+end
