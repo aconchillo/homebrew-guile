153f15a655b917e8bba96d5511d3dfb08ef14abc
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-06-26 16:30:51 -0700

    guile-xmlrpc: initial version 0.4.0

---
+++ Formula/guile-xmlrpc.rb
@@ -0,0 +1,42 @@
+class GuileXmlrpc < Formula
+  desc "XMLRPC module for Guile"
+  homepage "https://github.com/aconchillo/guile-xmlrpc"
+  url "https://download.savannah.gnu.org/releases/guile-xmlrpc/guile-xmlrpc-0.4.0.tar.gz"
+  sha256 "357755571b85a1e07d1caafdc63ad1c186913ec9ad94b44631df37c8abfc91ac"
+
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "guile"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+    EOS
+  end
+
+  test do
+    xmlrpc = testpath/"xmlrpc.scm"
+    xmlrpc.write <<~EOS
+      (use-modules (xmlrpc))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    system "guile", xmlrpc
+  end
+end
