d2118098ad7dd89756d1ac51aa16ef64d225ae11
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-06-30 16:46:18 -0700

    guile-dsv: initial version 0.3.0

---
+++ Formula/guile-dsv.rb
@@ -0,0 +1,49 @@
+class GuileDsv < Formula
+  desc "Guile module for working with the delimiter-separated values data format"
+  homepage "https://github.com/artyom-poptsov/guile-dsv"
+  url "https://github.com/artyom-poptsov/guile-dsv/archive/v0.3.0.tar.gz"
+  sha256 "4197964279a8791cb0e7368bd87925cb3ae5d5f1ce5bea898c006801a4525187"
+
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "guile"
+  depends_on "guile-lib"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    # We need this so we can find other modules.
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    inreplace buildpath/"configure.ac", "2.0 2.2", "3.0"
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+    EOS
+  end
+
+  test do
+    dsv = testpath/"dsv.scm"
+    dsv.write <<~EOS
+      (use-modules (dsv))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    system "guile", dsv
+  end
+end
