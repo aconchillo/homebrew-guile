3118ff07e8f3d2b386c03e61f45bbf7aad954aa6
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-06-30 17:10:57 -0700

    nyacc: initial version 1.02.2

---
+++ Formula/nyacc.rb
@@ -0,0 +1,43 @@
+class Nyacc < Formula
+  desc "Guile modules for generating parsers and lexical analyzers"
+  homepage "https://www.nongnu.org/nyacc/"
+  url "https://download.savannah.gnu.org/releases/nyacc/nyacc-1.02.2.tar.gz"
+  sha256 "152ed9f88e9fe3aafbe54495f35ca704ef593c1669a86ca87f4b5a620c083f4f"
+
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texinfo" => :build
+  depends_on "guile"
+  depends_on "guile-bytestructures"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+    EOS
+  end
+
+  test do
+    nyacc = testpath/"nyacc.scm"
+    nyacc.write <<~EOS
+      (use-modules (nyacc parse))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+
+    system "guile", nyacc
+  end
+end
