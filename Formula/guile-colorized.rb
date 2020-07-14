88d365b0a0d895c38c00be61006bbc22fc2f3bc4
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-07-13 18:46:23 -0700

    guile-colorized: initial version 0.1

---
+++ Formula/guile-colorized.rb
@@ -0,0 +1,41 @@
+class GuileColorized < Formula
+  desc "Colorized REPL for GNU Guile"
+  homepage "https://gitlab.com/NalaGinrut/guile-colorized"
+  url "https://gitlab.com/NalaGinrut/guile-colorized/-/archive/v0.1/guile-colorized-v0.1.tar.gz"
+  sha256 "fb60ca552f6e935b9d8b6a7cf9b2a4e1c85caa6b5bce53f6f4541acf21c5ab47"
+
+  bottle :unneeded
+
+  depends_on "guile"
+
+  def install
+    inreplace buildpath/"Makefile", "$(TARGET)", "$(DESTDIR)/share/guile/site/3.0/ice-9/"
+
+    mkdir "#{prefix}/share/guile/site/3.0/ice-9/"
+
+    system "make", "DESTDIR=#{prefix}", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+        export GUILE_SYSTEM_EXTENSIONS_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/extensions"
+    EOS
+  end
+
+  test do
+    colorized = testpath/"colorized.scm"
+    colorized.write <<~EOS
+      (use-modules (ice-9 colorized))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "guile", colorized
+  end
+end
