a245ac9401ee92b2720e0972c61aa3b6ba1dd6b4
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-07-17 18:55:24 -0700

    guile-jwt: initial version 0.2.0

---
+++ Formula/guile-jwt.rb
@@ -0,0 +1,44 @@
+class GuileJwt < Formula
+  desc "JSON Web Token module for Guile"
+  homepage "https://github.com/aconchillo/guile-jwt"
+  url "https://download.savannah.gnu.org/releases/guile-jwt/guile-jwt-0.2.0.tar.gz"
+  sha256 "c26ac2336bfb8ada5dd5e253ebdafa9f0a72bc4c34bdb79e917a0ec06098641a"
+
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "guile"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+        export GUILE_SYSTEM_EXTENSIONS_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/extensions"
+    EOS
+  end
+
+  test do
+    jwt = testpath/"jwt.scm"
+    jwt.write <<~EOS
+      (use-modules (jwt))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "guile", jwt
+  end
+end
