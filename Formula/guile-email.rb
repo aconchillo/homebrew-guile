7ec7388a3ef7b7f6a56adf7a2d47a1a360463a8d
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2020-11-21 02:17:35 +0000

    guile-email: add 0.2.2 bottle.

--- Formula/guile-email.rb
+++ Formula/guile-email.rb
@@ -4,6 +4,13 @@ class GuileEmail < Formula
   url "https://guile-email.systemreboot.net/releases/guile-email-0.2.2.tar.lz"
   sha256 "b38c6ab2f2534c15b29c7a23d5ffbc4f5ff8348af1d0c90af696bafd1cc888e5"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-email-0.2.2"
+    cellar :any_skip_relocation
+    sha256 "9dfe38426b56762281b314ad3ca8e01a1a1f88860e1a8bdf7029cb1fcdeb82be" => :catalina
+    sha256 "6a322746100c2d807e4efa7fbd98ecb6a537bc077bdc1087136d420351aaea79" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
