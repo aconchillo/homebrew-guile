81b02d6a3c19d33baa687c354f11d191feb83405
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2020-11-21 02:17:34 +0000

    skribilo: add 0.9.5 bottle.

--- Formula/skribilo.rb
+++ Formula/skribilo.rb
@@ -4,6 +4,11 @@ class Skribilo < Formula
   url "https://download.savannah.nongnu.org/releases/skribilo/skribilo-0.9.5.tar.gz"
   sha256 "00826a21c4634fb0b410ee89eb48068c445d800825874654e3d53d5ca3f0bf09"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/skribilo-0.9.5"
+    sha256 "9226187497325f69fa8cdd5994619efa44106d4bf5bfa4fbf7b2ebffcfc08495" => :catalina
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "gnu-sed" => :build
