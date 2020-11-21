223ca49480631d2c2616539fc42c6c120d956a1e
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2020-11-21 02:17:34 +0000

    guile-redis: add 2.0.0 bottle.

--- Formula/guile-redis.rb
+++ Formula/guile-redis.rb
@@ -4,6 +4,13 @@ class GuileRedis < Formula
   url "https://download.savannah.gnu.org/releases/guile-redis/guile-redis-2.0.0.tar.gz"
   sha256 "58b70a8af1c2720fbc2902b0d614c33fcbda0dc44128d23d38752d52c6fbf4c5"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-redis-2.0.0"
+    cellar :any_skip_relocation
+    sha256 "7fa9d85f87f1f606da86abc44904a2a28f3d479af5551b4fa36d42a7ca9a389d" => :catalina
+    sha256 "0a1e937cb1dd3a9f50ee459a61db69fc6e42d942f5bfaf91c3e578d70d192a03" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
