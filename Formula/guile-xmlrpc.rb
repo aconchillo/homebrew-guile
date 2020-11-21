b1658c2718b9f1c7b8bd9d65a8228c48d777a25d
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2020-11-21 02:17:34 +0000

    guile-xmlrpc: add 0.4.0 bottle.

--- Formula/guile-xmlrpc.rb
+++ Formula/guile-xmlrpc.rb
@@ -4,6 +4,13 @@ class GuileXmlrpc < Formula
   url "https://download.savannah.gnu.org/releases/guile-xmlrpc/guile-xmlrpc-0.4.0.tar.gz"
   sha256 "357755571b85a1e07d1caafdc63ad1c186913ec9ad94b44631df37c8abfc91ac"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-xmlrpc-0.4.0"
+    cellar :any_skip_relocation
+    sha256 "ad0f7bca8cb950a8bea0740ad630a7b0f11f93f8e09e6dcf90730ff79a2f0b30" => :catalina
+    sha256 "e982a7d30d738772772be0d0d9db6af6163f86e88b06ad67d19e984ce4d3555f" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
