cd72313c47c06697efcf5b3b99e0880194ccabc6
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2020-11-21 02:17:34 +0000

    guile-sqlite3: add 0.1.2 bottle.

--- Formula/guile-sqlite3.rb
+++ Formula/guile-sqlite3.rb
@@ -4,6 +4,13 @@ class GuileSqlite3 < Formula
   url "https://notabug.org/guile-sqlite3/guile-sqlite3/archive/v0.1.2.tar.gz"
   sha256 "dc88fbcd30b6eb7d6d275212fd68eb4ca7a45c9d31ffe4a3d706bd318f9d0016"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-sqlite3-0.1.2"
+    cellar :any_skip_relocation
+    sha256 "6dd873a9acb48be2510892aed88449dfcba6e6d616d8da1a5abd249e00da1a7c" => :catalina
+    sha256 "951093517fc1a8c289244c8e7d625a8dff423f2116a7b31e11e2cc0ad801fd71" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
