f1236c267c86578c27a53a44e0f1ebbf3d608b07
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2020-11-30 23:01:28 +0000

    guile-zlib: add 0.0.1 bottle.

--- Formula/guile-zlib.rb
+++ Formula/guile-zlib.rb
@@ -4,6 +4,13 @@ class GuileZlib < Formula
   url "https://notabug.org/guile-zlib/guile-zlib/archive/0.0.1.tar.gz"
   sha256 "f1100be6dd31b02983cf498155bf11155ca833421f99698f29e5694317335fb1"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-zlib-0.0.1"
+    cellar :any_skip_relocation
+    sha256 "933cdb91d44b56b2c840e2d4868fdd747ce4ca98426e76e4e967773b3ec52bd8" => :catalina
+    sha256 "4332ab1c54b7369fe1eceb15686b2714da1c0013a99aac3976adaa68dbb02c99" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
