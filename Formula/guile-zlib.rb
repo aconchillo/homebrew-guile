6fbadfbd719741c78570acfa2cca0622f372ae8e
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-11-30 14:12:43 -0800

    guile-zlib: initial version 0.0.1

---
+++ Formula/guile-zlib.rb
@@ -0,0 +1,43 @@
+class GuileZlib < Formula
+  desc "GNU Guile library providing bindings to zlib"
+  homepage "https://notabug.org/guile-zlib/guile-zlib"
+  url "https://notabug.org/guile-zlib/guile-zlib/archive/0.0.1.tar.gz"
+  sha256 "f1100be6dd31b02983cf498155bf11155ca833421f99698f29e5694317335fb1"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "guile"
+  depends_on "zlib"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+        export GUILE_SYSTEM_EXTENSIONS_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/extensions"
+    EOS
+  end
+
+  test do
+    zlib = testpath/"zlib.scm"
+    zlib.write <<~EOS
+      (use-modules (zlib))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "guile", zlib
+  end
+end
