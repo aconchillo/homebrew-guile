e3d1b6fa0b218f3067a2c3e46c9c1b0e1669a3e3
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2021-01-04 19:21:44 +0000

    guile-zstd: add 0.1.1 bottle.

--- Formula/guile-zstd.rb
+++ Formula/guile-zstd.rb
@@ -4,6 +4,12 @@ class GuileZstd < Formula
   url "https://notabug.org/guile-zstd/guile-zstd/archive/v0.1.1.tar.gz"
   sha256 "6e57ef524f20cab79ca5fd62366c5435f71cf652f582fc1e7d62585e90d499c5"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-zstd-0.1.1"
+    sha256 "fe5556b11ce2be358f7513a889e478cf8f509bed560d24379479bd0ad3b6849a" => :catalina
+    sha256 "3c4421053b47bd93d0fed69d1998a434967cf6057c3e5c660ffcefae91578918" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
