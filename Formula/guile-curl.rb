03a41ebaf925addcfbaaaed5c3e45f86cbac8cf9
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2021-01-11 03:08:43 +0000

    guile-curl: add 0.8 bottle.

--- Formula/guile-curl.rb
+++ Formula/guile-curl.rb
@@ -4,6 +4,13 @@ class GuileCurl < Formula
   url "https://github.com/spk121/guile-curl/archive/v0.8.tar.gz"
   sha256 "2a541af4281783c3e60f1fefee459b34d740bba3c844c308a2d615a228676c64"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-curl-0.8"
+    cellar :any
+    sha256 "8f8a8efcc4ab132e14929c92644a9b8b6d684aaced9464d2d3e7de3560daa58d" => :catalina
+    sha256 "84ea01a8f0d5bf780700f8bf9d8f25c5d0146d8635ee41323b5ec975bd626e28" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
