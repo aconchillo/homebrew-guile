2ed1539b9b3313af0039278351786519bc3cc700
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2021-02-05 07:08:12 +0000

    guile-jwt: add 0.2.0 bottle.

--- Formula/guile-jwt.rb
+++ Formula/guile-jwt.rb
@@ -4,6 +4,12 @@ class GuileJwt < Formula
   url "https://download.savannah.gnu.org/releases/guile-jwt/guile-jwt-0.2.0.tar.gz"
   sha256 "c26ac2336bfb8ada5dd5e253ebdafa9f0a72bc4c34bdb79e917a0ec06098641a"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-jwt-0.2.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, catalina: "ddd24d96ddafb30293034c3efcec9940202ba8823e8ae3a8db079fd21cd586a7"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
