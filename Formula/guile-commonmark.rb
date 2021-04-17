d822175e1c230c02445db713d3a9956513bef137
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2021-04-17 04:55:17 +0000

    guile-commonmark: add 0.1.2 bottle.

--- Formula/guile-commonmark.rb
+++ Formula/guile-commonmark.rb
@@ -4,6 +4,12 @@ class GuileCommonmark < Formula
   url "https://github.com/OrangeShark/guile-commonmark/releases/download/v0.1.2/guile-commonmark-0.1.2.tar.gz"
   sha256 "56d518ece5e5d94c1b24943366149e9cb0f6abdb24044c049c6c0ea563d3999e"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-commonmark-0.1.2"
+    sha256 cellar: :any_skip_relocation, catalina:     "f061ae24605c41b175a4d41ff2e2c99b6de004bf260b0cddfe45594e47ceb705"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c2cba15811b26624035ed23fcf5434bfcb5fb2f844185d7a0c3acc3bfe8d8ecc"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
