641233d13e04a4081fc800c9eeb2a99f7aa8f22e
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2021-04-17 05:08:17 +0000

    haunt: update 0.2.5 bottle.

--- Formula/haunt.rb
+++ Formula/haunt.rb
@@ -5,9 +5,9 @@ class Haunt < Formula
   sha256 "1324b7986897ca55b9150ae80f8241f4037075bc2bb5bbdae56385540d2dc4bf"
 
   bottle do
-    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/haunt-0.2.4"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, catalina: "5d347e7a1d2ee47d7663b430cc4025544561d4850aed7baf8c27dc3ca34aaa32"
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/haunt-0.2.5"
+    sha256 cellar: :any_skip_relocation, catalina:     "d8b6ee10d9ca5ec70daf4452c129e4f42a5d14094d4be104664ba2a774163cde"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c462fb97390e8218487d118afb8042f7655a764c85a31e02fe5f8abe49a71c46"
   end
 
   depends_on "autoconf" => :build
