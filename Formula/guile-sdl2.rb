c37e1e67b882b59e7445db157f9523a13292d0ac
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2021-04-21 06:50:01 +0000

    guile-sdl2: add 0.5.0 bottle.

--- Formula/guile-sdl2.rb
+++ Formula/guile-sdl2.rb
@@ -4,6 +4,12 @@ class GuileSdl2 < Formula
   url "https://files.dthompson.us/guile-sdl2/guile-sdl2-0.5.0.tar.gz"
   sha256 "9564cb72d3d40eabcdfd8f79a0e2fb2c5ddcce52c9150f5df67a7d771e031d85"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-sdl2-0.5.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "6e0033b84b339f32992b2db801fec195ecd4898ad0ac161a3b664b36ebf5a32c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ef2da2b38aa8d5933025cb0249aad4547b39ee8ee2cd4c29b184327b0d66dbb7"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
