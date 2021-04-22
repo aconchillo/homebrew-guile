4e853c59fa78f89e51209b48bb071eaeca260846
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2021-04-22 21:29:35 +0000

    guile-hall: add 0.3.1 bottle.

--- Formula/guile-hall.rb
+++ Formula/guile-hall.rb
@@ -4,6 +4,12 @@ class GuileHall < Formula
   url "https://gitlab.com/a-sassmannshausen/guile-hall/-/archive/0.3.1/guile-hall-0.3.1.tar.gz"
   sha256 "b8b1b7c9613217cb4df8ffe22a5e821c40cd5c9ce3de4ca6ad61fc9bb9e5f38e"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-hall-0.3.1"
+    sha256 cellar: :any_skip_relocation, catalina:     "af084f0b59eae905d64602dce7512bc35b9b160bd45147964bc7701859de7fd6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ce041bf4ab7d1c67e66f866e8f773405fd251d7ab6be6d20a7cf126d45947092"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
