d20595aecefeaaf5e14c7b572138bd66237de9f8
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2021-04-22 21:29:35 +0000

    guile-dbd-mysql: add 2.1.7 bottle.

--- Formula/guile-dbd-mysql.rb
+++ Formula/guile-dbd-mysql.rb
@@ -4,6 +4,12 @@ class GuileDbdMysql < Formula
   url "https://github.com/opencog/guile-dbi/archive/guile-dbi-2.1.7.tar.gz"
   sha256 "e337c242891221e2bf6da5433f4d5144c40b37da400a3a011c8ac07390516df4"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-dbd-mysql-2.1.7"
+    sha256 cellar: :any,                 catalina:     "b592cc92b10e69f26c52f2e352217c254fe014478d4224870c83408c172edfd2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7d4c430ae6b7654d1c7e924e68f6d4084f179582a8622871c128b1c2104a8e1a"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "guile"
