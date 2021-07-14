db9af31d115c959ca21ed7fadeae1579327ce19c
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2021-07-14 11:01:07 -0700

    guile-smc: update to version 0.2.0

--- Formula/guile-smc.rb
+++ Formula/guile-smc.rb
@@ -1,8 +1,8 @@
 class GuileSmc < Formula
   desc "GNU Guile state machine compiler"
   homepage "https://github.com/artyom-poptsov/guile-smc"
-  url "https://github.com/artyom-poptsov/guile-smc/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "60383bd441bc3636bd4dac6035464b1177eabc9f4229e2c447334a2e8c35f626"
+  url "https://github.com/artyom-poptsov/guile-smc/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "dc4df6d07886538ae88c1fc2397580c79aac93f4f80435649d903665efcf5b78"
 
   bottle do
     root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-smc-0.1.0"
@@ -15,8 +15,6 @@ class GuileSmc < Formula
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "guile"
-
-  patch :DATA
 
   def install
     system "autoreconf", "-vif"
@@ -47,50 +45,3 @@ class GuileSmc < Formula
     system "guile", smc
   end
 end
-
-__END__
-diff --git a/configure.ac b/configure.ac
-index 5e229ee..ba2a675 100644
---- a/configure.ac
-+++ b/configure.ac
-@@ -47,9 +47,8 @@ GUILE_PKG([3.0 2.2])
- GUILE_PROGS
- GUILE_SITE_DIR
-
--pkgdatadir="$datadir/$PACKAGE"
- if test "x$guilesitedir" = "x"; then
--   guilesitedir="$pkgdatadir"
-+   guilesitedir="$datadir/guile/site/$GUILE_EFFECTIVE_VERSION"
- fi
- AC_SUBST([guilesitedir])
-
-diff --git a/modules/smc/Makefile.am b/modules/smc/Makefile.am
-index 7270822..0a96147 100644
---- a/modules/smc/Makefile.am
-+++ b/modules/smc/Makefile.am
-@@ -31,7 +31,7 @@ GOBJECTS = $(SOURCES:%.scm=%.go)
- moddir=$(guilesitedir)/smc/
- nobase_dist_mod_DATA = $(SOURCES)
-
--ccachedir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache
-+ccachedir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache/smc
- nobase_nodist_ccache_DATA = $(GOBJECTS)
-
- # Make sure source files are installed first, so that the mtime of
-diff --git a/modules/smc/core/log.scm b/modules/smc/core/log.scm
-index c2062fa..b94b0e0 100644
---- a/modules/smc/core/log.scm
-+++ b/modules/smc/core/log.scm
-@@ -45,10 +45,10 @@
-
- (define (log level fmt . args)
-   (let* ((message (apply format #f fmt args))
--         (command (format #f "~a ~a --priority=user.~a --tag='~a' '~a'"
-+         (command (format #f "~a ~a -p 'user.~a' -t '~a' '~a'"
-                           %logger
-                           (if *use-stderr?*
--                              "--stderr"
-+                              "-s"
-                               "")
-                           level
-                           %tag
