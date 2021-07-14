c4dbf897f7b12bfde23023a5bcf659c5ca8c15f7
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2021-07-14 11:19:25 -0700

    guile-ini: update to version 0.2.0

--- Formula/guile-ini.rb
+++ Formula/guile-ini.rb
@@ -1,8 +1,8 @@
 class GuileIni < Formula
   desc "GNU Guile library for working with INI format"
   homepage "https://github.com/artyom-poptsov/guile-ini"
-  url "https://github.com/artyom-poptsov/guile-ini/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "fd78c93b143f8a1c8f96b97c47ab43a781277327e7af274b777ac5fdc1c7dce8"
+  url "https://github.com/artyom-poptsov/guile-ini/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "04cf0393554b10a7247f145c365c3f2003cc97c7ab4634d45069ba529b9e7152"
 
   bottle do
     root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-ini-0.1.0"
@@ -15,8 +15,6 @@ class GuileIni < Formula
   depends_on "pkg-config" => :build
   depends_on "guile"
   depends_on "guile-smc"
-
-  patch :DATA
 
   def install
     ENV["GUILE_AUTO_COMPILE"] = "0"
@@ -54,35 +52,3 @@ class GuileIni < Formula
     system "guile", ini
   end
 end
-
-__END__
-diff --git a/configure.ac b/configure.ac
-index faf70ee..e53b6dd 100644
---- a/configure.ac
-+++ b/configure.ac
-@@ -47,9 +47,10 @@ GUILE_PKG([3.0 2.2])
- GUILE_PROGS
- GUILE_SITE_DIR
-
--pkgdatadir="$datadir/$PACKAGE"
-+GUILE_MODULE_REQUIRED([smc fsm])
-+
- if test "x$guilesitedir" = "x"; then
--   guilesitedir="$pkgdatadir"
-+   guilesitedir="$datadir/guile/site/$GUILE_EFFECTIVE_VERSION"
- fi
- AC_SUBST([guilesitedir])
-
-diff --git a/modules/ini/Makefile.am b/modules/ini/Makefile.am
-index 792ea6b..3815961 100644
---- a/modules/ini/Makefile.am
-+++ b/modules/ini/Makefile.am
-@@ -32,7 +32,7 @@ GOBJECTS = $(SOURCES:%.scm=%.go)
- moddir=$(guilesitedir)/ini/
- nobase_dist_mod_DATA = $(SOURCES) fsm.puml
-
--ccachedir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache
-+ccachedir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache/ini
- nobase_nodist_ccache_DATA = $(GOBJECTS)
-
- # Make sure source files are installed first, so that the mtime of
