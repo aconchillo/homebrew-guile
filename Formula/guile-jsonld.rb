6ac2c8c6340e8c858ac4182e9df420c822b3b140
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2021-07-31 15:04:11 -0700

    guile-jsonld: initial version 1.0.2

---
+++ Formula/guile-jsonld.rb
@@ -0,0 +1,50 @@
+class GuileJsonld < Formula
+  desc "Implementation of the JsonLD algorithms"
+  homepage "https://framagit.org/tyreunom/guile-jsonld"
+  url "https://framagit.org/tyreunom/guile-jsonld/-/archive/1.0.2/guile-jsonld-1.0.2.tar.gz"
+  sha256 "100d36654e6ed84b80ba62a5293dc880655dd738feaf5db16391161e89891af1"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gnutls"
+  depends_on "guile"
+  depends_on "guile-json"
+  depends_on "guile-rdf"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    # We need this so we can find other modules.
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "./bootstrap"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+        export GUILE_SYSTEM_EXTENSIONS_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/extensions"
+    EOS
+  end
+
+  test do
+    jsonld = testpath/"jsonld.scm"
+    jsonld.write <<~EOS
+      (use-modules (jsonld json))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "guile", jsonld
+  end
+end
