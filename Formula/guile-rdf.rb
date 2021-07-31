a40f830b86ed84cde552abd96666bad4d5a38c59
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2021-07-31 18:10:18 +0000

    guile-rdf: add 1.0 bottle.

--- Formula/guile-rdf.rb
+++ Formula/guile-rdf.rb
@@ -4,6 +4,12 @@ class GuileRdf < Formula
   url "https://framagit.org/tyreunom/guile-rdf/-/archive/1.0/guile-rdf-1.0.tar.gz"
   sha256 "11dcd2b970f962c484260540ca77d5bb983ae99bab81127aea5fb59c3e851de0"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-rdf-1.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "761d617928e64b44ebef23c6f99c2ff6569862f387b1def94afd203380c7e83f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "455394c7df2a20bf3ea2fbb0d7ebd9f05cc51603eb7d121237dec20921849358"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
