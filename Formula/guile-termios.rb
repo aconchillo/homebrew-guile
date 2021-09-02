f3012e7717b7be8aa6945aefcffe39d5f2cbc839
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2021-09-02 01:38:45 +0000

    guile-termios: add 0.6.4 bottle.

--- Formula/guile-termios.rb
+++ Formula/guile-termios.rb
@@ -4,6 +4,12 @@ class GuileTermios < Formula
   url "https://github.com/ft/guile-termios/archive/refs/tags/v0.6.4.tar.gz"
   sha256 "4e6751788ed8cb2ec8426da32e6bcfe68cc9f4fe34ce8a12d93ca289e33bbe1e"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-termios-0.6.4"
+    sha256 cellar: :any_skip_relocation, catalina:     "bab1e837496e81d7b29f461ab3137800fe79fc204c2c7e8b43b93051782ba44f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "78f1e956e189fb3a6d31f88de9061b97fc1bb7df891fef4858b724e836883047"
+  end
+
   depends_on "guile"
 
   def install
