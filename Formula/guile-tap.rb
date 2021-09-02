87a57e09858291e7b8c5c9b3ae2a3ba75839a4a4
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2021-09-02 01:27:29 +0000

    guile-tap: add 0.4.3 bottle.

--- Formula/guile-tap.rb
+++ Formula/guile-tap.rb
@@ -4,6 +4,12 @@ class GuileTap < Formula
   url "https://github.com/ft/guile-tap/archive/refs/tags/v0.4.3.tar.gz"
   sha256 "07e44d1a597b21fe6f2e0ec012e196da0b61dea7066e59a0fd93c62a5dcad575"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-tap-0.4.3"
+    sha256 cellar: :any_skip_relocation, catalina:     "778973d50c2865a8fc2307a066b77bb9de22895498f2c520d3d59eb971d83441"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "598c451258d240f857d251ee9c2493454d8e823d832a15533cc48781fb96b8b7"
+  end
+
   depends_on "guile"
 
   def install
