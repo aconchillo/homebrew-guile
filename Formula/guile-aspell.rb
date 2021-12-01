738df0e0c1a33f04393b7b634645f2fdb1acf4b0
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2021-11-30 22:55:05 -0800

    guile-aspell: initial version 0.5.0

---
+++ Formula/guile-aspell.rb
@@ -0,0 +1,43 @@
+class GuileAspell < Formula
+  desc "Spellcheck Library for Guile"
+  homepage "https://github.com/spk121/guile-aspell"
+  url "https://github.com/spk121/guile-aspell/releases/download/0.5.0/guile_aspell-0.5.0.tar.gz"
+  sha256 "958e3f1f80bffdc5200778804259384722b94ed0fda5957001fc652f941322a7"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texinfo" => :build
+  depends_on "aspell"
+  depends_on "guile"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+        export GUILE_SYSTEM_EXTENSIONS_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/extensions"
+    EOS
+  end
+
+  test do
+    aspell = testpath/"aspell.scm"
+    aspell.write <<~EOS
+      (use-modules (aspell))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "guile", aspell
+  end
+end
