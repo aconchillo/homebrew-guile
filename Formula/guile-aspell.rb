f6213774c2b9b77ff1195c319cdfd0bf54e2e03c
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2021-12-01 07:29:04 +0000

    guile-aspell: add 0.5.0 bottle.

--- Formula/guile-aspell.rb
+++ Formula/guile-aspell.rb
@@ -4,6 +4,12 @@ class GuileAspell < Formula
   url "https://github.com/spk121/guile-aspell/releases/download/0.5.0/guile_aspell-0.5.0.tar.gz"
   sha256 "958e3f1f80bffdc5200778804259384722b94ed0fda5957001fc652f941322a7"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-aspell-0.5.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "0f82210328fc2fc42c918d07580e6b96bcea94eb7e8a7ddea5ddd8c2069c8182"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7a58e2c0dbccad7ce6f3f71818c2247f074b9bea19a4fdeed9cdf984f2b3921d"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
