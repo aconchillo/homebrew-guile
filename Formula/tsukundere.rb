dcc10f41a2a02cbc9aa1de34c1558e231a051ad9
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2022-01-08 00:39:21 -0800

    tsukundere: 0.4.2 revision 2 (fix audio)
    Closes #80.
    
    Signed-off-by: Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>

--- Formula/tsukundere.rb
+++ Formula/tsukundere.rb
@@ -3,7 +3,7 @@ class Tsukundere < Formula
   homepage "https://gitlab.com/lilyp/tsukundere"
   url "https://gitlab.com/lilyp/tsukundere/-/archive/0.4.2/tsukundere-0.4.2.tar.gz"
   sha256 "ac4cdabd448b389154e0e392e620340c3e5da8a23e205c855fa36fa4b506d7ba"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://github.com/aconchillo/homebrew-guile/releases/download/tsukundere-0.4.2_1"
@@ -89,7 +89,7 @@ index 65a2f0e..ef606b5 100644
 +++ b/baka/markup.scm
 @@ -21,11 +21,7 @@
    #:export (parse-markup append-markup))
-
+ 
  (eval-when (compile load eval)
 -  (load-extension
 -   (if (getenv "LIBBAKA_UNINSTALLED")
@@ -97,7 +97,7 @@ index 65a2f0e..ef606b5 100644
 -       "@tsukundere_extensiondir@/libbaka.so")
 -   "baka_init_scm_markup"))
 +  (load-extension "libbaka" "baka_init_scm_markup"))
-
+ 
  ;; Split @var{markup} into text and attributes, appending the former to
  ;; @var{text} and the latter to @var{attributes}.
 diff --git a/baka/text.scm.in b/baka/text.scm
@@ -109,7 +109,7 @@ index 11cb4db..d0b5e24 100644
 +++ b/baka/text.scm
 @@ -41,11 +41,7 @@
              add-font-dir!))
-
+ 
  (eval-when (compile load eval)
 -  (load-extension
 -   (if (getenv "LIBBAKA_UNINSTALLED")
@@ -117,7 +117,7 @@ index 11cb4db..d0b5e24 100644
 -       "@tsukundere_extensiondir@/libbaka.so")
 -   "baka_init_scm_text"))
 +  (load-extension "libbaka" "baka_init_scm_text"))
-
+ 
  (define %double4 (list double double double double))
  (define %paint-options-structure (list int int %double4 %double4 double))
 diff --git a/configure.ac b/configure.ac
@@ -126,12 +126,12 @@ index 1a4bbb9..f8f8039 100644
 +++ b/configure.ac
 @@ -60,7 +60,6 @@ PKG_CHECK_MODULES(PANGOCAIRO, [pangocairo])
  PKG_CHECK_MODULES(SDL2, [sdl2])
-
+ 
  AC_CONFIG_FILES([Makefile po/Makefile.in
 -                 baka/markup.scm baka/text.scm
                   tsukundere/i18n.scm])
  AC_CONFIG_FILES([pre-inst-env], [chmod +x pre-inst-env])
-
+ 
 diff --git a/pre-inst-env.in b/pre-inst-env.in
 index 75ba9fd..7200f16 100644
 --- a/pre-inst-env.in
@@ -139,12 +139,41 @@ index 75ba9fd..7200f16 100644
 @@ -27,8 +27,7 @@ export GUILE_EXTENSIONS_PATH GUILE_LOAD_COMPILED_PATH GUILE_LOAD_PATH
  GUILE_AUTO_COMPILE=0
  export GUILE_AUTO_COMPILE
-
+ 
 -LIBBAKA_UNINSTALLED=1
  TSUKUNDERE_UNINSTALLED=1
 -export LIBBAKA_UNINSTALLED TSUKUNDERE_UNINSTALLED
 +export TSUKUNDERE_UNINSTALLED
-
+ 
  exec "$@"
---
+diff --git a/tsukundere/game.scm b/tsukundere/game.scm
+index 4f7b357..2ae4b49 100644
+--- a/tsukundere/game.scm
++++ b/tsukundere/game.scm
+@@ -337,17 +337,18 @@ in the 'text layer using SPEAKER_FONT and TEXT_FONT respectively."
+       (when locale-dir
+         (bindtextdomain game-name locale-dir))))
+ 
+-  (load-preferences)
+-  (save-preferences #f 'unless-exists?)
+-  (and=> (current-game) load-preferences)
+-
+-  (fluid-set! *save-state* save)
+   (sdl-init)
+   (set-hint! 'render-scale-quality 2)
+   (when use-mixer?
+     (init-sound! #:mixer-formats mixer-formats
+                  #:mixer-frequency mixer-frequency
+                  #:mixer-chunk-size mixer-chunk-size))
++
++  (load-preferences)
++  (save-preferences #f 'unless-exists?)
++  (and=> (current-game) load-preferences)
++
++  (fluid-set! *save-state* save)
+   (call-with-window (make-window #:title window-title
+                                  #:size (list window-width window-height)
+                                  #:resizable? #t
+-- 
 2.34.1
+
