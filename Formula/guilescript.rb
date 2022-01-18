43dd02c9e7a187914f026d80b62b7bf1b267536a
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2022-01-17 22:00:07 -0800

    guilescript: initial version 0.0.0
    Closes #81.
    
    Signed-off-by: Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>

---
+++ Formula/guilescript.rb
@@ -0,0 +1,42 @@
+class Guilescript < Formula
+  desc "Guile to JavaScript compiler"
+  homepage "https://github.com/aconchillo/guilescript"
+  url "https://github.com/aconchillo/guilescript/archive/refs/tags/v0.0.0.tar.gz"
+  sha256 "54e47f3c4ce79b4d37b768e171a574a3cf851802c61e54a4332200d46c869136"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "guile"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+        export GUILE_SYSTEM_EXTENSIONS_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/extensions"
+    EOS
+  end
+
+  test do
+    guilescript = testpath/"guilescript.scm"
+    guilescript.write <<~EOS
+      (use-modules (language guilescript compile))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "guile", guilescript
+  end
+end
