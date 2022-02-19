69a84ebca60b995c4f16dcd7a7ff33875bf29396
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2022-02-19 07:28:33 +0000

    guile-newt: add 0.0.2 bottle.

--- Formula/guile-newt.rb
+++ Formula/guile-newt.rb
@@ -4,6 +4,12 @@ class GuileNewt < Formula
   url "https://gitlab.com/mothacehe/guile-newt/-/archive/0.0.2/guile-newt-0.0.2.tar.gz"
   sha256 "b74f44eb13c3edee001e5198fcd0b63e5e2b0c3a57b18d3c792a5014b092c237"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-newt-0.0.2"
+    sha256 cellar: :any_skip_relocation, big_sur:      "e0f819581eb6d70556e317d171633da5903c736f43c3ee393d83cc80abaa0af5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f930223d70681a8ea791aa46c03dbf02d85776b9dfca80d5e4af965afe6a0b6e"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
