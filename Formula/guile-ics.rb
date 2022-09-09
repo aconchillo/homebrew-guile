9d2a143c7b90a4d8204e8980d7f3106dd5ce89ce
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2022-09-09 10:14:01 -0700

    guile-ics: update to version 0.3.0

--- Formula/guile-ics.rb
+++ Formula/guile-ics.rb
@@ -1,8 +1,8 @@
 class GuileIcs < Formula
   desc "RFC5545 (iCalendar) format parser for GNU Guile"
   homepage "https://github.com/artyom-poptsov/guile-ics"
-  url "https://github.com/artyom-poptsov/guile-ics/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "707bfce74b5f466e3ab64fca7567265a6e5a04787a00dc5901ba0b949948f67a"
+  url "https://github.com/artyom-poptsov/guile-ics/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "6d628dbeb53bc56c4e447415408fcb3f37d8c685c81d685477172f57b207b483"
 
   bottle do
     root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-ics-0.2.1"
@@ -12,11 +12,14 @@ class GuileIcs < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  # coreutils because of install not working great on macOS 10.11. coreutils
+  # will bring ginstall.
+  depends_on "coreutils" => :build
   depends_on "help2man" => :build
   depends_on "pkg-config" => :build
   depends_on "texinfo" => :build
   depends_on "guile"
-  depends_on "guile-lib"
+  depends_on "guile-smc"
 
   def install
     ENV["GUILE_AUTO_COMPILE"] = "0"
