212a512ad36d7fa1499662cab4a07ed328ecd5d1
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2022-09-23 15:08:07 -0700

    guile-ssh: update to version 0.16.0

--- Formula/guile-ssh.rb
+++ Formula/guile-ssh.rb
@@ -1,15 +1,8 @@
 class GuileSsh < Formula
   desc "SSH library for programs written in Guile"
   homepage "https://github.com/artyom-poptsov/guile-ssh"
-  url "https://github.com/artyom-poptsov/guile-ssh/archive/v0.15.1.tar.gz"
-  sha256 "434fdfdcd439d038c15e40290a8d3187837ad969577573baacb7b105c0c3628f"
-  revision 1
-
-  bottle do
-    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-ssh-0.15.1_1"
-    sha256 cellar: :any,                 big_sur:      "80b6cafe853b7b75b703ab761564a2f4864914e98fc564748c306222950635b1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "026e88b4e902c290189f94da84ace68661b980d4edd74f6ebe9c0b730c7937e4"
-  end
+  url "https://github.com/artyom-poptsov/guile-ssh/archive/v0.16.0.tar.gz"
+  sha256 "32e1b0ba3d6252bd7ac4261e4c10efb82bd3cabc978498d96c78050a92e1db8e"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -58,24 +51,11 @@ index ca8c6fe..c710305 100644
 @@ -15,7 +15,9 @@
  ## You should have received a copy of the GNU General Public License
  ## along with Guile-SSH.  If not, see <http://www.gnu.org/licenses/>.
- 
+
 -lib_LTLIBRARIES = libguile-ssh.la
 +guileextensiondir = $(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/extensions
 +
 +guileextension_LTLIBRARIES = libguile-ssh.la
- 
+
  libguile_ssh_la_SOURCES = \
  	auth.c \
-diff --git a/configure.ac b/configure.ac
-index fc47793..c455a06 100644
---- a/configure.ac
-+++ b/configure.ac
-@@ -126,7 +126,7 @@ if test "x$have_srfi64" != "xyes"; then
- fi
- 
- LT_INIT()
--p
-+
- if test "x$guilesitedir" = "x"; then
-    guilesitedir="$datadir/guile/site/$GUILE_EFFECTIVE_VERSION"
- fi
