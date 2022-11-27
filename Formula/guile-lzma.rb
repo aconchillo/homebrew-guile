9f801d8b39b9c6f9739ccc63cbeb2ace9e565793
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2022-11-27 10:51:24 -0800

    guile-lzma: new version 0.1.1
    Closes #146.
    
    Signed-off-by: Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>

---
+++ Formula/guile-lzma.rb
@@ -0,0 +1,53 @@
+class GuileLzma < Formula
+  desc "GNU Guile bindings to the XZ compression library"
+  homepage "https://ngyro.com/software/guile-lzma.html"
+  url "https://files.ngyro.com/guile-lzma/guile-lzma-0.1.1.tar.gz"
+  sha256 "2b866896d672ed4d39008f4b5336750d7897560a06678365f5c5a72bd2fcce5e"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "guile"
+  depends_on "guile-bytestructures"
+  depends_on "xz"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    # We need this so we can find other modules.
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def post_install
+    # Touch config.go to avoid Guile recompilation.
+    touch "#{lib}/guile/3.0/site-ccache/lzma/config.go"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+        export GUILE_SYSTEM_EXTENSIONS_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/extensions"
+    EOS
+  end
+
+  test do
+    lzma = testpath/"lzma.scm"
+    lzma.write <<~EOS
+      (use-modules (lzma))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "guile", lzma
+  end
+end
