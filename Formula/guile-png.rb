fd165e74b91be5487843cedb6b1edbac124644a2
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2022-12-18 12:35:34 -0800

    guile-png: new version 0.3.0
    Closes #157.
    
    Signed-off-by: Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>

--- Formula/guile-png.rb
+++ Formula/guile-png.rb
@@ -1,8 +1,8 @@
 class GuilePng < Formula
   desc "Portable Network Graphics library for GNU Guile"
   homepage "https://github.com/artyom-poptsov/guile-png"
-  url "https://github.com/artyom-poptsov/guile-png/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "b8b1a046461033d85f55cc4f7946faf1d20a0c62baaf280fa7083df144f9236f"
+  url "https://github.com/artyom-poptsov/guile-png/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "2b5aa4c06ef1807d86ffa9a395653bd793560e2a8ef20c3eac1181fd8384bb55"
 
   bottle do
     root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-png-0.2.0"
@@ -18,8 +18,6 @@ class GuilePng < Formula
   depends_on "guile-lib"
   depends_on "guile-smc"
   depends_on "guile-zlib"
-
-  patch :DATA
 
   def install
     ENV["GUILE_AUTO_COMPILE"] = "0"
@@ -57,134 +55,3 @@ class GuilePng < Formula
     system "guile", png
   end
 end
-
-__END__
-diff --git a/modules/Makefile.am b/modules/Makefile.am
-index 0ab8a6f..05788ce 100644
---- a/modules/Makefile.am
-+++ b/modules/Makefile.am
-@@ -17,16 +17,16 @@
- ## You should have received a copy of the GNU General Public License
- ## along with Guile-PNG.  If not, see <http://www.gnu.org/licenses/>.
- 
--include $(top_srcdir)/build-aux/am/guilec
-+include $(top_srcdir)/build-aux/am/guile.am
- 
- SUBDIRS = \
--	png	\
-+	png \
- 	.
- 
- SOURCES = \
- 	png.scm
- 
--moddir=$(prefix)/share/guile/site/$(GUILE_EFFECTIVE_VERSION)/
--godir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache/
-+moddir=$(datadir)/guile/site/$(GUILE_EFFECTIVE_VERSION)
-+godir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache
- 
- ### Makefile.am ends here.
-diff --git a/modules/png/Makefile.am b/modules/png/Makefile.am
-index 5b0dc81..97a52ed 100644
---- a/modules/png/Makefile.am
-+++ b/modules/png/Makefile.am
-@@ -30,12 +30,12 @@ SOURCES = \
- 	image-processing.scm	\
- 	graphics.scm
- 
--moddir=$(guilesitedir)/png/
-+moddir=$(datadir)/guile/site/$(GUILE_EFFECTIVE_VERSION)/png
- nobase_dist_mod_DATA = \
- 	$(SOURCES) \
- 	$(EXTRA_DIST)
- 
--ccachedir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache/png/
-+ccachedir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache/png
- nobase_nodist_ccache_DATA = $(GOBJECTS)
- 
- # Make sure source files are installed first, so that the mtime of
-diff --git a/modules/png/core/Makefile.am b/modules/png/core/Makefile.am
-index a8d1ac7..5dbb13d 100644
---- a/modules/png/core/Makefile.am
-+++ b/modules/png/core/Makefile.am
-@@ -37,11 +37,11 @@ SOURCES = \
- 
- GOBJECTS = $(SOURCES:%.scm=%.go)
- 
--moddir=$(guilesitedir)/png/core/
-+moddir=$(datadir)/guile/site/$(GUILE_EFFECTIVE_VERSION)/png/core
- nobase_dist_mod_DATA = \
- 	$(SOURCES)
- 
--ccachedir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache/png/core/
-+ccachedir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache/png/core
- nobase_nodist_ccache_DATA = $(GOBJECTS)
- 
- # Make sure source files are installed first, so that the mtime of
-diff --git a/modules/png/fsm/Makefile.am b/modules/png/fsm/Makefile.am
-index b57b2fb..0529704 100644
---- a/modules/png/fsm/Makefile.am
-+++ b/modules/png/fsm/Makefile.am
-@@ -25,21 +25,26 @@ SOURCES = \
- 	signature-context.scm
- 
- BUILT_SOURCES =	\
--	context.scm
-+	context.scm \
-+	png-parser.scm \
-+	chunk-parser.scm \
-+	signature-parser.scm
-+
-+INSTALL += \
-+	context.scm \
-+	png-parser.scm \
-+	chunk-parser.scm \
-+	signature-parser.scm
- 
- EXTRA_DIST += \
- 	chunk-parser.puml \
--	chunk-parser.scm \
- 	png-parser.puml \
--	png-parser.scm	\
--	signature-parser.puml \
--	signature-parser.scm
-+	signature-parser.puml
- 
--INSTALL += \
--	context.scm
-+moddir=$(datadir)/guile/site/$(GUILE_EFFECTIVE_VERSION)/png/fsm
-+godir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache/png/fsm
- 
--moddir=$(prefix)/share/guile/site/$(GUILE_EFFECTIVE_VERSION)/png/fsm/
--godir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache/png/fsm/
-+SUFFIXES = .puml
- 
- GUILE_SMC_CONTEXT_ARGS = \
- 	--log-driver file \
-@@ -86,8 +91,6 @@ endif
- 
- GUILE_SMC_ENV = GUILE_AUTO_COMPILE=0
- 
--all: context.scm png-parser.scm
--
- context.scm:
- 	@echo "  SMC      $@"
- 	@$(GUILE_SMC_ENV) smc context $(GUILE_SMC_CONTEXT_ARGS) > $@
-diff --git a/modules/png/graphics/Makefile.am b/modules/png/graphics/Makefile.am
-index 4f4cc4a..47797b3 100644
---- a/modules/png/graphics/Makefile.am
-+++ b/modules/png/graphics/Makefile.am
-@@ -31,11 +31,11 @@ SOURCES = \
- 
- GOBJECTS = $(SOURCES:%.scm=%.go)
- 
--moddir=$(guilesitedir)/png/core/
-+moddir=$(datadir)/guile/site/$(GUILE_EFFECTIVE_VERSION)/png/graphics
- nobase_dist_mod_DATA = \
- 	$(SOURCES)
- 
--ccachedir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache/png/graphics/
-+ccachedir=$(libdir)/guile/$(GUILE_EFFECTIVE_VERSION)/site-ccache/png/graphics
- nobase_nodist_ccache_DATA = $(GOBJECTS)
- 
- # Make sure source files are installed first, so that the mtime of
