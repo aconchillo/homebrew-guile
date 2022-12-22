6b4521d83024912d69b02104e12740ac2abc536e
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2022-12-22 22:30:57 +0000

    guile-goblins: add 0.8 bottle.

--- Formula/guile-goblins.rb
+++ Formula/guile-goblins.rb
@@ -4,6 +4,12 @@ class GuileGoblins < Formula
   url "https://gitlab.com/spritely/guile-goblins/-/archive/v0.8/guile-goblins-v0.8.tar.gz"
   sha256 "f9db90d892960b3696576210773bf194bd3a49d43991e70f161552ed6c3af442"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-goblins-0.8"
+    sha256 cellar: :any_skip_relocation, monterey:     "ba6c0c22e09cd91874bc69edae1afd6c8466a4f2f2c8460fd77c034d0eb21d2f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9b609a34187ae62cdca14ea46e7ccb5bc3b988ce1cd1cf9d0974f74d44c519c4"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "texinfo" => :build
