d7988ef39d4472ebb59d037d60c557aced3ee153
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2022-12-22 14:18:43 -0800

    guile-goblins: new version 0.8.0

---
+++ Formula/guile-goblins.rb
@@ -0,0 +1,49 @@
+class GuileGoblins < Formula
+  desc "Distributed object programming environment"
+  homepage "https://gitlab.com/spritely/guile-goblins"
+  url "https://gitlab.com/spritely/guile-goblins/-/archive/v0.8/guile-goblins-v0.8.tar.gz"
+  sha256 "f9db90d892960b3696576210773bf194bd3a49d43991e70f161552ed6c3af442"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "texinfo" => :build
+  depends_on "fibers"
+  depends_on "guile"
+  depends_on "guile-gcrypt"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    # We need this so we can find other modules.
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "./bootstrap.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+        export GUILE_SYSTEM_EXTENSIONS_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/extensions"
+    EOS
+  end
+
+  test do
+    goblins = testpath/"goblins.scm"
+    goblins.write <<~EOS
+      (use-modules (goblins))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "guile", goblins
+  end
+end
