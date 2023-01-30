74808f3273abd2643ec0c828cad4d9add519f2ba
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2023-01-30 13:18:24 -0800

    guile-goblins: new version 0.10
    Closes #174.
    
    Signed-off-by: Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>

--- Formula/guile-goblins.rb
+++ Formula/guile-goblins.rb
@@ -1,8 +1,8 @@
 class GuileGoblins < Formula
   desc "Distributed object programming environment"
   homepage "https://gitlab.com/spritely/guile-goblins"
-  url "https://gitlab.com/spritely/guile-goblins/-/archive/v0.8/guile-goblins-v0.8.tar.gz"
-  sha256 "f9db90d892960b3696576210773bf194bd3a49d43991e70f161552ed6c3af442"
+  url "https://spritely.institute/files/releases/guile-goblins/guile-goblins-0.10.tar.gz"
+  sha256 "a2f52decdc51484a912d029b713ab4d70261e6b8404f616b3e42824a22afdf8e"
 
   bottle do
     root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-goblins-0.8"
@@ -10,8 +10,6 @@ class GuileGoblins < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "9b609a34187ae62cdca14ea46e7ccb5bc3b988ce1cd1cf9d0974f74d44c519c4"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
   depends_on "texinfo" => :build
   depends_on "fibers"
   depends_on "guile"
@@ -25,7 +23,6 @@ class GuileGoblins < Formula
     ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
     ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
 
-    system "./bootstrap.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
