12ca44a8ed7acdd04ef906853c4bfe3d3dd6e2cd
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2023-05-01 14:46:47 -0700

    guile-ini: new version 0.5.3
    Closes #182.
    
    Signed-off-by: Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>

---
+++ Formula/guile-ini.rb
@@ -0,0 +1,55 @@
+class GuileIni < Formula
+  desc "GNU Guile library for working with INI format"
+  homepage "https://github.com/artyom-poptsov/guile-ini"
+  url "https://github.com/artyom-poptsov/guile-ini/archive/v0.5.3.tar.gz"
+  sha256 "9f31c9ee57a4823ec1635718242908ae0bad9ced563a21d469189f26418ce06e"
+
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-ini-0.5.2_1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "fb129463d56056549b8b295af2a173b6ffb94f246d3f12530da990aec58a1f22"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "coreutils" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texinfo" => :build
+  depends_on "guile"
+  depends_on "guile-smc"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    # We need this so we can find other modules.
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+        export GUILE_SYSTEM_EXTENSIONS_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/extensions"
+    EOS
+  end
+
+  test do
+    ini = testpath/"ini.scm"
+    ini.write <<~EOS
+      (use-modules (ini))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "guile", ini
+  end
+end
