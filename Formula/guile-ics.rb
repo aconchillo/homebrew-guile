0e42bc76ad3969e3838ac03687727753dc97e0c9
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2023-05-01 23:50:41 +0000

    guile-ics: update 0.5.0 bottle.

--- Formula/guile-ics.rb
+++ Formula/guile-ics.rb
@@ -5,9 +5,9 @@ class GuileIcs < Formula
   sha256 "3f4d8deca5ca13ae4927dd4866092b4375b01cf1300fc93fafc98a28e8ce7de6"
 
   bottle do
-    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-ics-0.4.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "2193c06561588d4cff4a7ca0780d0bcda586b850a6125e736a1c6aff2d6ec9ec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "f805c4f8a568fd39902429db8f6f61523dc6a8be79a2574223b185249720919d"
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-ics-0.5.0"
+    sha256 cellar: :any_skip_relocation, monterey:     "7478f502ccc402e2bf4eecaac2e1fd8035d0cd2766d22ec6ec6307b3cb8110db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e5fd89396705b6488e83ca6c0b9b8cd686d879c73ac38b4dec042647e828ad0a"
   end
 
   depends_on "autoconf" => :build
