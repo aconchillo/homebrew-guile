fb2bf5c452dc52a74ff2c63b6d6b67a80670e77f
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2023-06-12 15:17:57 -0700

    guile-deck: 0.2.0-1 depend on guile-gnutls
    Closes #197.
    
    Signed-off-by: Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>

--- Formula/guile-deck.rb
+++ Formula/guile-deck.rb
@@ -3,6 +3,7 @@ class GuileDeck < Formula
   homepage "https://github.com/artyom-poptsov/guile-deck"
   url "https://github.com/artyom-poptsov/guile-deck/archive/refs/tags/v0.2.0.tar.gz"
   sha256 "a2b3a5cd7626010abef1ec217f8a70dac0eea734d0aa6e34a3f5819b4eaafe4b"
+  revision 1
 
   bottle do
     root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-deck-0.2.0"
@@ -13,9 +14,9 @@ class GuileDeck < Formula
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "texinfo" => :build
-  depends_on "gnutls"
   depends_on "guile"
   depends_on "guile-gcrypt"
+  depends_on "guile-gnutls"
   depends_on "guile-json"
 
   patch :DATA
