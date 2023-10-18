0651711d60b0a24497a81160ea9bf2891cc66984
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2023-10-18 09:27:36 -0700

    guile-next: new version 3.0.9-79e836

---
+++ Formula/guile-next.rb
@@ -0,0 +1,89 @@
+class GuileNext < Formula
+  desc "GNU Ubiquitous Intelligent Language for Extensions"
+  homepage "https://www.gnu.org/software/guile/"
+  url "https://git.savannah.gnu.org/git/guile.git", revision: "79e836b8cc601a1259c934000a953a8d739ddd6f"
+  version "3.0.9"
+  license "LGPL-3.0-or-later"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gettext" => :build
+  depends_on "gnu-sed" => :build
+  depends_on "bdw-gc"
+  depends_on "gmp"
+  depends_on "libtool"
+  depends_on "libunistring"
+  depends_on "pkg-config" # guile-config is a wrapper around pkg-config.
+  depends_on "readline"
+
+  uses_from_macos "flex" => :build
+  uses_from_macos "gperf"
+  uses_from_macos "libffi", since: :catalina
+  uses_from_macos "libxcrypt"
+
+  on_system :linux, macos: :ventura_or_newer do
+    depends_on "texinfo" => :build
+  end
+
+  def install
+    # Avoid superenv shim
+    inreplace "meta/guile-config.in", "@PKG_CONFIG@", Formula["pkg-config"].opt_bin/"pkg-config"
+
+    system "./autogen.sh"
+
+    system "./configure", *std_configure_args,
+                          "--with-libreadline-prefix=#{Formula["readline"].opt_prefix}",
+                          "--with-libgmp-prefix=#{Formula["gmp"].opt_prefix}",
+                          "--disable-nls"
+    system "make", "install"
+
+    # A really messed up workaround required on macOS --mkhl
+    Pathname.glob("#{lib}/*.dylib") do |dylib|
+      lib.install_symlink dylib.basename => "#{dylib.basename(".dylib")}.so"
+    end
+
+    # This is either a solid argument for guile including options for
+    # --with-xyz-prefix= for libffi and bdw-gc or a solid argument for
+    # Homebrew automatically removing Cellar paths from .pc files in favour
+    # of opt_prefix usage everywhere.
+    inreplace lib/"pkgconfig/guile-3.0.pc" do |s|
+      s.gsub! Formula["bdw-gc"].prefix.realpath, Formula["bdw-gc"].opt_prefix
+      s.gsub! Formula["libffi"].prefix.realpath, Formula["libffi"].opt_prefix if MacOS.version < :catalina
+    end
+
+    (share/"gdb/auto-load").install Dir["#{lib}/*-gdb.scm"]
+  end
+
+  def post_install
+    # Create directories so installed modules can create links inside.
+    (HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache").mkpath
+    (HOMEBREW_PREFIX/"lib/guile/3.0/extensions").mkpath
+    (HOMEBREW_PREFIX/"share/guile/site/3.0").mkpath
+  end
+
+  def caveats
+    <<~EOS
+      Guile libraries can now be installed here:
+          Source files: #{HOMEBREW_PREFIX}/share/guile/site/3.0
+        Compiled files: #{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache
+            Extensions: #{HOMEBREW_PREFIX}/lib/guile/3.0/extensions
+
+      Add the following to your .bashrc or equivalent:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+        export GUILE_SYSTEM_EXTENSIONS_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/extensions"
+    EOS
+  end
+
+  test do
+    hello = testpath/"hello.scm"
+    hello.write <<~EOS
+      (display "Hello World")
+      (newline)
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    system bin/"guile", hello
+  end
+end
