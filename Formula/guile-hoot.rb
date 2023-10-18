ecd0de676ebe17b840140c74b38d257a2a236537
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2023-10-18 10:52:07 -0700

    guile-hoot: new version 0.1.0
    Closes #207.
    
    Signed-off-by: Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>

---
+++ Formula/guile-hoot.rb
@@ -0,0 +1,48 @@
+class GuileHoot < Formula
+  desc "Full-featured WebAssembly (WASM) toolkit in Scheme"
+  homepage "https://spritely.institute/hoot/"
+  url "https://spritely.institute/files/releases/guile-hoot/guile-hoot-0.1.0.tar.gz"
+  sha256 "2f81c178a46e62057ef01c41f5e39a83091c1c32a24a85177366422124cb0781"
+
+  depends_on "texinfo" => :build
+  depends_on "guile-next"
+
+  def install
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    # We need this so we can find other modules.
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      To use `guile-hoot` you need to unlink `guile` and then link `guile-next`:
+        brew unlink guile
+        brew link guile-next
+
+      Remember to add the following to your .bashrc or equivalent in order to use this module:
+        export GUILE_LOAD_PATH="#{HOMEBREW_PREFIX}/share/guile/site/3.0"
+        export GUILE_LOAD_COMPILED_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/site-ccache"
+        export GUILE_SYSTEM_EXTENSIONS_PATH="#{HOMEBREW_PREFIX}/lib/guile/3.0/extensions"
+    EOS
+  end
+
+  test do
+    hoot = testpath/"hoot.scm"
+    hoot.write <<~EOS
+      (use-modules (hoot compile))
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+    ENV["GUILE_LOAD_PATH"] = HOMEBREW_PREFIX/"share/guile/site/3.0"
+    ENV["GUILE_LOAD_COMPILED_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/site-ccache"
+    ENV["GUILE_SYSTEM_EXTENSIONS_PATH"] = HOMEBREW_PREFIX/"lib/guile/3.0/extensions"
+
+    system "guile", hoot
+  end
+end
