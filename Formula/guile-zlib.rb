201b2ae62158c4fbce7139c9fcd587ecf277d49a
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2023-12-01 10:00:58 -0800

    guile-zlib: rebuilt bottles
    Closes #214.
    
    Signed-off-by: Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>

--- Formula/guile-zlib.rb
+++ Formula/guile-zlib.rb
@@ -3,7 +3,7 @@ class GuileZlib < Formula
   homepage "https://notabug.org/guile-zlib/guile-zlib"
   url "https://notabug.org/guile-zlib/guile-zlib/archive/v0.1.0.tar.gz"
   sha256 "25c726b570a06d21bc6fd7ec6093f377c749ce2efdd1d1516ac1b595f3f94ee9"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-zlib-0.1.0_1"
