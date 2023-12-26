16ce7510b3b5191601c5ba53d59e5f0184690b66
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2023-12-25 17:18:41 -0800

    fibers: 1.3.1 revision 1 (sonoma runner)

--- Formula/fibers.rb
+++ Formula/fibers.rb
@@ -3,6 +3,7 @@ class Fibers < Formula
   homepage "https://github.com/wingo/fibers"
   url "https://github.com/wingo/fibers/releases/download/v1.3.1/fibers-1.3.1.tar.gz"
   sha256 "a5e1a9c49c0efe7ac6f355662041430d4b64e59baa538d2b8fb5ef7528d81dbf"
+  revision 1
 
   bottle do
     root_url "https://github.com/aconchillo/homebrew-guile/releases/download/fibers-1.3.1"
