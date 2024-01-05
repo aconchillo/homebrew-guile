86a91093e41197d9da2144dcdb167eafb0538e4f
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2024-01-05 20:14:27 +0000

    ploticus: add 2.42 bottle.

--- Formula/ploticus.rb
+++ Formula/ploticus.rb
@@ -4,6 +4,13 @@ class Ploticus < Formula
   url "https://downloads.sourceforge.net/project/ploticus/ploticus/2.42/ploticus242_src.tar.gz"
   sha256 "3f29e4b9f405203a93efec900e5816d9e1b4381821881e241c08cab7dd66e0b0"
 
+  bottle do
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/ploticus-2.42"
+    sha256 arm64_sonoma: "a07ba5cf7c465f72d5f2183113a77a9d4d01b32295332a094ace5a841d01a716"
+    sha256 ventura:      "470e18c8b029406e11ac7946ad99e922757065a0aefb29464974471ee05f1e25"
+    sha256 x86_64_linux: "69a3d4125b2539865bfdcda78871346132f0db668b20c660e7be0c5ceec88a01"
+  end
+
   depends_on "libpng"
 
   def install
