3e6b3a47b0aad771dd2e570886ae89f5aae61f2e
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2024-02-20 18:32:45 -0800

    haunt: new version 0.3.0
    Closes #252.
    
    Signed-off-by: Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>

--- Formula/haunt.rb
+++ Formula/haunt.rb
@@ -1,9 +1,8 @@
 class Haunt < Formula
   desc "Simple, functional, hackable static site generator"
   homepage "https://dthompson.us/projects/haunt.html"
-  url "https://files.dthompson.us/haunt/haunt-0.2.6.tar.gz"
-  sha256 "bcf28b43d84325d8a61005f02011a045195e7ccf3fefbc4542823ed09fe590db"
-  revision 1
+  url "https://files.dthompson.us/haunt/haunt-0.3.0.tar.gz"
+  sha256 "98babed06be54a066c3ebc94410a91eb7cc48367e94d528131d3ba271499992b"
 
   bottle do
     root_url "https://github.com/aconchillo/homebrew-guile/releases/download/haunt-0.2.6_1"
@@ -18,8 +17,6 @@ class Haunt < Formula
   depends_on "guile"
   depends_on "guile-commonmark"
   depends_on "guile-reader"
-
-  patch :DATA
 
   def install
     ENV["GUILE_AUTO_COMPILE"] = "0"
@@ -57,120 +54,3 @@ class Haunt < Formula
     system "guile", haunt
   end
 end
-
-__END__
-diff --git a/Makefile.am b/Makefile.am
-index e3eb96f..1bddaa4 100644
---- a/Makefile.am
-+++ b/Makefile.am
-@@ -46,7 +46,6 @@ bin_SCRIPTS =					\
- SOURCES =					\
-   haunt/config.scm				\
-   haunt/utils.scm				\
--  haunt/inotify.scm				\
-   haunt/post.scm				\
-   haunt/page.scm				\
-   haunt/asset.scm				\
-@@ -81,6 +80,12 @@ SOURCES +=					\
-
- endif
-
-+if HAVE_INOTIFY
-+
-+SOURCES += haunt/inotify.scm
-+
-+endif
-+
- TESTS =						\
-   tests/helper.scm				\
-   tests/post.scm				\
-diff --git a/configure.ac b/configure.ac
-index bb9550c..2a80cdf 100644
---- a/configure.ac
-+++ b/configure.ac
-@@ -21,4 +21,7 @@ AM_CONDITIONAL([HAVE_GUILE_READER], [test "x$have_guile_reader" = "xyes"])
- GUILE_MODULE_AVAILABLE([have_guile_commonmark], [(commonmark)])
- AM_CONDITIONAL([HAVE_GUILE_COMMONMARK], [test "x$have_guile_commonmark" = "xyes"])
-
-+AC_CHECK_FUNCS(inotify_init)
-+AM_CONDITIONAL([HAVE_INOTIFY], [test "x$HAVE_INOTIFY_INIT" = "xyes"])
-+
- AC_OUTPUT
-diff --git a/haunt/ui/serve.scm b/haunt/ui/serve.scm
-index 62fbc6b..8511d87 100644
---- a/haunt/ui/serve.scm
-+++ b/haunt/ui/serve.scm
-@@ -30,7 +30,6 @@
-   #:use-module (ice-9 ftw)
-   #:use-module (ice-9 threads)
-   #:use-module (haunt config)
--  #:use-module (haunt inotify)
-   #:use-module (haunt serve web-server)
-   #:use-module (haunt site)
-   #:use-module (haunt ui)
-@@ -90,6 +89,17 @@ Start an HTTP server for the current site.~%")
-
- ;; TODO: Detect new directories and watch them, too.
- (define (watch/linux config-file check-dir? check-file?)
-+  ;; Lazy load inotify procedures.  Requiring the module in the
-+  ;; define-module definition would cause crashes on non-Linux
-+  ;; platforms where the FFI cannot bind to inotify functions.
-+  (define make-inotify (@ (haunt inotify) make-inotify))
-+  (define inotify-add-watch! (@ (haunt inotify) inotify-add-watch!))
-+  (define inotify-pending-events? (@ (haunt inotify) inotify-pending-events?))
-+  (define inotify-read-event (@ (haunt inotify) inotify-read-event))
-+  (define inotify-watch-file-name (@ (haunt inotify) inotify-watch-file-name))
-+  (define inotify-event-watch (@ (haunt inotify) inotify-event-watch))
-+  (define inotify-event-file-name (@ (haunt inotify) inotify-event-file-name))
-+  (define inotify-event-type (@ (haunt inotify) inotify-event-type))
-   (let ((inotify (make-inotify)))
-     (define (no-op name stat result) result)
-     (define (watch-directory name stat result)
-diff --git a/configure.ac b/configure.ac
-index 2a80cdf..da381d3 100644
---- a/configure.ac
-+++ b/configure.ac
-@@ -21,7 +21,6 @@ AM_CONDITIONAL([HAVE_GUILE_READER], [test "x$have_guile_reader" = "xyes"])
- GUILE_MODULE_AVAILABLE([have_guile_commonmark], [(commonmark)])
- AM_CONDITIONAL([HAVE_GUILE_COMMONMARK], [test "x$have_guile_commonmark" = "xyes"])
-
--AC_CHECK_FUNCS(inotify_init)
--AM_CONDITIONAL([HAVE_INOTIFY], [test "x$HAVE_INOTIFY_INIT" = "xyes"])
-+AC_CHECK_FUNC([inotify_init], [AM_CONDITIONAL(HAVE_INOTIFY, true)], [AM_CONDITIONAL(HAVE_INOTIFY, false)])
-
- AC_OUTPUT
-diff --git a/haunt/ui/serve.scm b/haunt/ui/serve.scm
-index 8511d87..8abe0c6 100644
---- a/haunt/ui/serve.scm
-+++ b/haunt/ui/serve.scm
-@@ -89,17 +89,21 @@ Start an HTTP server for the current site.~%")
-
- ;; TODO: Detect new directories and watch them, too.
- (define (watch/linux config-file check-dir? check-file?)
--  ;; Lazy load inotify procedures.  Requiring the module in the
-+  ;; Lazy load inotify module.  Requiring the module in the
-   ;; define-module definition would cause crashes on non-Linux
-   ;; platforms where the FFI cannot bind to inotify functions.
--  (define make-inotify (@ (haunt inotify) make-inotify))
--  (define inotify-add-watch! (@ (haunt inotify) inotify-add-watch!))
--  (define inotify-pending-events? (@ (haunt inotify) inotify-pending-events?))
--  (define inotify-read-event (@ (haunt inotify) inotify-read-event))
--  (define inotify-watch-file-name (@ (haunt inotify) inotify-watch-file-name))
--  (define inotify-event-watch (@ (haunt inotify) inotify-event-watch))
--  (define inotify-event-file-name (@ (haunt inotify) inotify-event-file-name))
--  (define inotify-event-type (@ (haunt inotify) inotify-event-type))
-+  (define inotify-module (resolve-module '(haunt inotify)))
-+  (define make-inotify (module-ref inotify-module 'make-inotify))
-+  (define inotify-add-watch! (module-ref inotify-module 'inotify-add-watch!))
-+  (define inotify-pending-events?
-+    (module-ref inotify-module 'inotify-pending-events?))
-+  (define inotify-read-event (module-ref inotify-module 'inotify-read-event))
-+  (define inotify-watch-file-name
-+    (module-ref inotify-module 'inotify-watch-file-name))
-+  (define inotify-event-watch (module-ref inotify-module 'inotify-event-watch))
-+  (define inotify-event-file-name
-+    (module-ref inotify-module 'inotify-event-file-name))
-+  (define inotify-event-type (module-ref inotify-module 'inotify-event-type))
-   (let ((inotify (make-inotify)))
-     (define (no-op name stat result) result)
-     (define (watch-directory name stat result)
