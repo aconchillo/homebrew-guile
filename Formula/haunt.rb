aa030e2b38abc06eba1e27882fe41e6d47056bd3
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2024-02-24 07:00:14 +0000

    haunt: update 0.3.0_1 bottle.

--- Formula/haunt.rb
+++ Formula/haunt.rb
@@ -6,10 +6,10 @@ class Haunt < Formula
   revision 1
 
   bottle do
-    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/haunt-0.3.0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "1d0860dee1d57633094b5477b59b856a0f276c2c689fe4016e46f0a2713c05cb"
-    sha256 cellar: :any_skip_relocation, ventura:      "d4c7156f162fc9bb2ac1a7e963af196963e2ae60210f375cbc7cdebc5c013a1a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "eab1dbfc841f9a67217df3838216a9c35f04fc93ce76b434efdeadca886e8859"
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/haunt-0.3.0_1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "0b205f94fb650bb78688c7090ddfecc0c24fa4fd203153a264c2a40febc7db36"
+    sha256 cellar: :any_skip_relocation, ventura:      "aaf9a6afd7b9dc42646a3c7df6fe57e5641d07860b20efbde3acb7e1222b2f7a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a64b835492291623e9664bb9b4e56f3e110aeec56f39a7e452ccb748162c5322"
   end
 
   depends_on "pkg-config" => :build
