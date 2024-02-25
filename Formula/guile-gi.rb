57fa2b551ef27530a2c9585b6604a5f96451a4e0
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2024-02-25 02:01:25 +0000

    guile-gi: update 0.3.2_2 bottle.

--- Formula/guile-gi.rb
+++ Formula/guile-gi.rb
@@ -6,9 +6,10 @@ class GuileGi < Formula
   revision 2
 
   bottle do
-    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-gi-0.3.2_1"
-    sha256 monterey:     "5a56819f824f43648dfd8c6e7d43c36f9b97408f0527e052640e8d9f1efccfec"
-    sha256 x86_64_linux: "eba1da1ea593f0818d0e03da541b002b03e2931c6b5c08d870c42a074975672f"
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-gi-0.3.2_2"
+    sha256 arm64_sonoma: "b7740b723bca04fbf7e6dcffff2c85873c4d8bdcb67227aba18584c879918f95"
+    sha256 ventura:      "854910107edd64fc94266993dced4f52a05ccd505250285b3d3de2ae3008b58e"
+    sha256 x86_64_linux: "394b4bd8bdc623f93feeb0b4dace485a3364c66761d660e8179c26839d2121a7"
   end
 
   depends_on "pkg-config" => :build
