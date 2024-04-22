330b5b5d16471a28b88fcc18f490ca678e0216a2
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2024-04-22 18:42:55 +0000

    guile-lib: update 0.2.8 bottle.

--- Formula/guile-lib.rb
+++ Formula/guile-lib.rb
@@ -5,10 +5,10 @@ class GuileLib < Formula
   sha256 "64e902ee0cbb2cee1efb8168c3bf9ed49e0b32a8a23db863e83bf14817d767d9"
 
   bottle do
-    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-lib-0.2.7_3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "1cd0941c77c48b809e41f22f81564588127bc635d0f146b8aa534d221ee2ff88"
-    sha256 cellar: :any_skip_relocation, ventura:      "0844c5bea425817237ee9bc0f599a6174660d7c27eb7204eaed53f0a1150791d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c7935813492bf578b1a4120b3738d0e8afff0365d2a142373590b9200624ba1e"
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-lib-0.2.8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "b8ae3cc8ff891db3f7ded4a470aec5339983d45dc73869244fa8b2f85526ef62"
+    sha256 cellar: :any_skip_relocation, ventura:      "8321728205df22a895eb2a458893d6dca19e176aa6e3a80bca7b0f47c620fc8a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "00ad9680ce606db33aaa69f8c714632488567ccc38db19366eaefae4fe362686"
   end
 
   depends_on "autoconf" => :build
