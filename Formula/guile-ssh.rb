9624a8d6a7d3b174197f42b99d9273aa88e1e0b0
Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>
2024-05-03 16:51:19 +0000

    guile-ssh: update 0.17.0 bottle.

--- Formula/guile-ssh.rb
+++ Formula/guile-ssh.rb
@@ -5,10 +5,10 @@ class GuileSsh < Formula
   sha256 "ce6670a2114d3269c2f79153adc8d6bd251d7c649fee0f02f5e893104a1b38bc"
 
   bottle do
-    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-ssh-0.16.4_1"
-    sha256 cellar: :any,                 arm64_sonoma: "3f2cb831d46823ef8474a8bd1924063b27141897201653581226efb8846e7070"
-    sha256 cellar: :any,                 ventura:      "82d66e2285d23e240a3fb0477aeed5e9c9d798a11f9760df5384dd2b9e0d9b73"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c95ccec4c4f7c60355d5d436892db5c6e3be17ea05d73c68a73aae624e603789"
+    root_url "https://github.com/aconchillo/homebrew-guile/releases/download/guile-ssh-0.17.0"
+    sha256 cellar: :any,                 arm64_sonoma: "9bc9e9cd82b9ddf53707979aa95c0cd4f5274f797853fce6acc70a7448be8ae8"
+    sha256 cellar: :any,                 ventura:      "65aa9ab82588ea81eda8b31444165f327c6b6452046a412f60b753e4aa54ab2d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9bca192f00bcdd63cf71862a3bd005f8fa463e48f3c1f877d62d6a977b8643d1"
   end
 
   depends_on "autoconf" => :build
