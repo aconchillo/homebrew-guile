4b96aa2ff65fd34eee77eddeb1eec11cf401475c
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2025-09-06 18:56:21 -0700

    fibers: update to 1.4.0
    Closes #348.
    
    Signed-off-by: Aleix Conchillo Flaqué <951761+aconchillo@users.noreply.github.com>

--- Formula/fibers.rb
+++ Formula/fibers.rb
@@ -1,9 +1,8 @@
 class Fibers < Formula
   desc "Concurrency library for Guile"
-  homepage "https://github.com/wingo/fibers"
-  url "https://github.com/wingo/fibers/releases/download/v1.3.1/fibers-1.3.1.tar.gz"
-  sha256 "a5e1a9c49c0efe7ac6f355662041430d4b64e59baa538d2b8fb5ef7528d81dbf"
-  revision 2
+  homepage "https://codeberg.org/fibers/fibers"
+  url "https://codeberg.org/fibers/fibers/archive/v1.4.0.tar.gz"
+  sha256 "2b9fa0bf3009111678a82343b8847aa0eed72dd9d1516fece7bc8274893a40c4"
 
   bottle do
     root_url "https://github.com/aconchillo/homebrew-guile/releases/download/fibers-1.3.1_2"
@@ -12,12 +11,17 @@ class Fibers < Formula
     sha256 x86_64_linux: "a0808b2e2e9d6b46008fb0426aada2c5c43cd89c229513621070f86da6c026b7"
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texinfo" => :build
   depends_on "guile"
   depends_on "libevent"
 
   def install
     ENV["GUILE_AUTO_COMPILE"] = "0"
 
+    system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
